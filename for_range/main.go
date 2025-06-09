5cb529f68d797135882fe2381978bc040f1d5c9c
gyorgy <gyorgy@testmail>
2025-06-09 11:19:25 +0200

    add foreach with channel

--- for_range/main.go
+++ for_range/main.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	nums := [5]int{1, 2, 3, 4, 5}
+	// nums := [5]int{1, 2, 3, 4, 5}
 
-	for i, item := range nums {
-		fmt.Println(i, item)
+	// for i, item := range nums {
+	// 	fmt.Println(i, item)
+	// }
+
+	ch := make(chan int, 5)
+	ch <- 100
+	ch <- 200
+	close(ch)
+
+	for val := range ch {
+		fmt.Println(val)
 	}
 }
